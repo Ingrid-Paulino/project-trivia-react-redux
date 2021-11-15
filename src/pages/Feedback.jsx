bf16803f6da94b24684b81899280ed18eff32849
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2021-11-15 15:12:51 -0300

    deletado testes e readme

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.playAgain = this.playAgain.bind(this);
   }
 
@@ -13,16 +13,44 @@ class Feedback extends Component {
     history.push('/');
   }
 
+  renderFeedBack() {
+    const storageUser = JSON.parse(localStorage.getItem('state'));
+    const { player: { assertions } } = storageUser;
+    if (assertions <= 0) {
+      return (
+        <>
+          <h3 data-testid="feedback-total-question">
+            {assertions}
+          </h3>
+          <h3>Não acertou nenhuma pergunta</h3>
+        </>
+      );
+    }
+    return (
+      <>
+        <h3>Acertou</h3>
+        <h3 data-testid="feedback-total-question">
+          {assertions}
+        </h3>
+        <h3>perguntas</h3>
+      </>
+    );
+  }
+
   render() {
     const NUMBER = 3;
     const msgCool = 'Mandou bem!';
     const msgBad = 'Podia ser melhor...';
     const storageUser = JSON.parse(localStorage.getItem('state'));
-    const { player: { assertions } } = storageUser;
+    const { player: { assertions, score } } = storageUser;
+    console.log(assertions, score);
+
     return (
       <div>
         <Header />
         <h3 data-testid="feedback-text">{ assertions < NUMBER ? msgBad : msgCool }</h3>
+        { this.renderFeedBack() }
+        <p data-testid="feedback-total-score">{`Um total de ${score} pontos`}</p>
         <button
           type="button"
           data-testid="btn-play-again"
@@ -40,5 +68,4 @@ Feedback.propTypes = {
     push: PropTypes.func.isRequired,
   }).isRequired,
 };
-
 export default Feedback;
